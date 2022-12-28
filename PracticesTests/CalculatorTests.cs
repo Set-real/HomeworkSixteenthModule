dffe91f48b8251dd07ba60a77b633d09a166a434
Name <email@example.ru>
2022-12-28 17:10:40 +0300

    ferst commit

---
+++ PracticesTests/CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Practices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustNotNullVallue()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(80, calculator.Additional(50, 30));
+        }
+        [Test]
+        public void SubtractionlMustNotNullVallue() 
+        { 
+            var calculator = new Calculator();
+            Assert.That
+        }
+    }
+}
